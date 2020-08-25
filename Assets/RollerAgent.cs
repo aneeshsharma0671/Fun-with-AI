e13303982afdf8839f9836addbf6de63338f6d0b
Aneesh sharma <aneeshsharma0671@gmail.com>
2020-08-25 10:26:14 +0530

    Added Trained Jump model

--- Assets/RollerAgent.cs
+++ Assets/RollerAgent.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using System.ComponentModel;
 
 public class RollerAgent : Agent
 {
     Rigidbody rB;
+    bool isjumping = false;
+    public float jumptime = 0.5f;
+    private float time = 0.0f;
     void Start()
     {
         rB = GetComponent<Rigidbody>();
@@ -18,9 +22,9 @@ public class RollerAgent : Agent
         {
             this.rB.angularVelocity = Vector3.zero;
             this.rB.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+            this.transform.localPosition = new Vector3(0, 0.5f, -4.0f);
         }
-        Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+        Target.localPosition = new Vector3(Random.value * 8-4, 0.5f, Random.value * 4);
     }
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -28,14 +32,18 @@ public class RollerAgent : Agent
         sensor.AddObservation(this.transform.localPosition);
         sensor.AddObservation(rB.velocity.x);
         sensor.AddObservation(rB.velocity.y);
+        sensor.AddObservation(rB.velocity.z);
     }
     public float forceMultiplier = 10;
     public override void OnActionReceived(float[] vectorAction)
     {
+        float jump=0f;
         Vector3 controlSignal = Vector3.zero;
         controlSignal.x = vectorAction[0];
         controlSignal.z = vectorAction[1];
+        jump = vectorAction[2];
         rB.AddForce(controlSignal * forceMultiplier);
+        rB.AddForce(jump *1f * Vector3.up,ForceMode.Impulse);
 
         float distancetotarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
         if(distancetotarget < 1.42f)
@@ -52,5 +60,21 @@ public class RollerAgent : Agent
     {
         actionsOut[0] = Input.GetAxis("Horizontal");
         actionsOut[1] = Input.GetAxis("Vertical");
+        if(this.transform.localPosition.y < 1f)
+        {
+            isjumping = false;
+        }
+        else
+        {
+            isjumping = true;
+        }
+        if (isjumping)
+        {
+            actionsOut[2] = 0;
+        }
+        if(Input.GetKey(KeyCode.Space)&& !isjumping)
+        {
+            actionsOut[2] = 1;
+        }
     }
 }
